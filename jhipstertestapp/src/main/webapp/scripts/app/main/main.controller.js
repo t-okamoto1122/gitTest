edc0a0d10d7a9b6a87e6a3c6172af060282e5e21
t-okamoto1122 <t-okamoto@begood-tech.com>
2015-06-05 11:35:42 +0900

    編集→コミット→編集→コミット→編集→コミットおよびプッシュ

--- jhipstertestapp/src/main/webapp/scripts/app/main/main.controller.js
+++ jhipstertestapp/src/main/webapp/scripts/app/main/main.controller.js
@@ -4,7 +4,7 @@ angular.module('jhipstertestappApp')
     .controller('MainController', function ($scope, Principal) {
         Principal.identity().then(function(account) {
             $scope.account = account;
-            consol.log("ああああああコミット後編集");
+            consol.log("ああああああコミット後編集後→コミット＆プッシュ");
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
     });
