01abc42cd4c3456b80ebe7ab86c3d65f0b294160
t-okamoto1122 <t-okamoto@begood-tech.com>
2015-06-05 11:40:29 +0900

    push after edit after comit after edit&comit&push

--- jhipstertestapp/src/main/webapp/scripts/app/main/main.controller.js
+++ jhipstertestapp/src/main/webapp/scripts/app/main/main.controller.js
@@ -4,7 +4,8 @@ angular.module('jhipstertestappApp')
     .controller('MainController', function ($scope, Principal) {
         Principal.identity().then(function(account) {
             $scope.account = account;
-            consol.log("ああああああコミット後編集後→コミット＆プッシュ→編集＆コミット");
+            consol.log("ああああああコミット後編集後→コミット＆プッシュ→編集＆コミット"
+            		+"んで、もっかい編集してコミット");
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
     });
