7d47370a2cbade21a31f0470987635c84d49141d
t-okamoto1122 <t-okamoto@begood-tech.com>
2015-06-05 11:29:55 +0900

    変更１

--- jhipstertestapp/src/main/webapp/scripts/app/main/main.controller.js
+++ jhipstertestapp/src/main/webapp/scripts/app/main/main.controller.js
@@ -4,6 +4,7 @@ angular.module('jhipstertestappApp')
     .controller('MainController', function ($scope, Principal) {
         Principal.identity().then(function(account) {
             $scope.account = account;
+            consol.log("ああああああ");
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
     });
